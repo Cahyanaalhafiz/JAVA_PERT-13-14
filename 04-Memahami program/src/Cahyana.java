c529781ebe16ad9f3866e55f5f0337d7d88a65fd
Santi31Sulistiawati <santi31sulistiawati@gmail.com>
2019-12-12 14:04:56 +0700

    first commit

---
+++ 04-Memahami program/src/Cahyana.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cahyana_a2.1900029
+ */
+public class Cahyana {
+
+	public static void main(String[] args){
+
+		System.out.print("Nama ku adalah Santi, teman si inov");
+
+	}
+}
